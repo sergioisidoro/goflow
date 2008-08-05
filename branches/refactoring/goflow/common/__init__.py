4db2eefb4ada2c80e0e6ca5455ef925278ed5c5d
alia_khouri@yahoo.com <alia_khouri@yahoo.com@6b16b2ad-a84a-0410-851a-4d2ecdfd7f99>
2008-08-05 20:57:11 +0000

    added some doctests

--- branches/refactoring/goflow/common/__init__.py
+++ branches/refactoring/goflow/common/__init__.py
@@ -2,7 +2,18 @@
 
 def get_safe_builtins():
     '''returns a safe for eval local dict of functions from __builtins__
-    and the math module. 
+    and the math module.
+    
+    >>> sorted(get_safe_builtins().keys())
+    ['False', 'None', 'True', 'abs', 'acos', 'all', 'any', 'apply', 'asin', 'atan', 
+     'atan2', 'basestring', 'bool', 'ceil', 'cos', 'cosh', 'degrees', 'dict', 'e', 
+     'enumerate', 'exp', 'fabs', 'filter', 'float', 'floor', 'fmod', 'frexp', 
+     'frozenset', 'getattr', 'hasattr', 'hash', 'hypot', 'int', 'iter', 'ldexp', 
+     'len', 'list', 'log', 'log10', 'long', 'map', 'max', 'min', 'modf', 'pi', 'pow', 
+     'radians', 'range', 'reduce', 'repr', 'reversed', 'round', 'set', 'setattr', 
+     'sin', 'sinh', 'slice', 'sorted', 'sqrt', 'str', 'sum', 'tan', 'tanh', 'tuple',
+      'unicode', 'xrange', 'zip']
+
     '''
     safe_builtins=[
         'False', 'None', 'True', 'abs', 'all', 'any', 'apply', 'basestring',
@@ -24,7 +35,13 @@ _locals = get_safe_builtins()
 def safe_eval(cond, **kwds):
     '''safe deferred evaluation of python expressions
     
-       note: must be used in combination with safe_builtins
+    (note: must be used in combination with safe_builtins)
+       
+    usage::
+       
+        >>> age = 10
+        >>> safe_eval('age > 5', age=age)()
+        True
     '''
     globals = {"__builtins__": None}
     _locals.update(kwds)
@@ -33,6 +50,13 @@ def safe_eval(cond, **kwds):
 
 def get_obj(modpath, obj=None):
     '''dynamic module/class/object importer
+    
+    usage::
+    
+        >>> join = get_obj('os.path', 'join')
+        >>> join('a', 'b')
+        'a/b'
+
     '''
     module = __import__(modpath, globals(), locals(), fromlist=['*'])
     if obj:
