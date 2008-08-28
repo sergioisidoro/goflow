baff1f549c7aef8afc9249620dd02f370ee48f77
alia_khouri@yahoo.com <alia_khouri@yahoo.com@6b16b2ad-a84a-0410-851a-4d2ecdfd7f99>
2008-08-28 02:22:40 +0000

    syncing with django-trunk admin changes

--- branches/refactoring/goflow/runtime/admin.py
+++ branches/refactoring/goflow/runtime/admin.py
@@ -9,21 +9,20 @@ class ProcessInstanceAdmin(admin.ModelAdmin):
     date_hierarchy = 'creation_time'
     list_display = ('title', 'process', 'user', 'creation_time', 'status')
     list_filter = ('process', 'user')
+admin.site.register(ProcessInstance, ProcessInstanceAdmin)
 
 class WorkItemAdmin(admin.ModelAdmin):
     date_hierarchy = 'date'
     list_display = ('date', 'user', 'instance', 'activity', 'status')
     list_filter = ('user', 'activity', 'status')
+admin.site.register(WorkItem, WorkItemAdmin)
 
 class EventAdmin(admin.ModelAdmin):
     date_hierarchy = 'date'
     list_display = ('date', 'name', 'workitem')
+admin.site.register(Event, EventAdmin)
 
 class DefaultAppModelAdmin(admin.ModelAdmin):
     list_display = ('__unicode__',)
-
-
-# register models for admin
-for m in [ProcessInstance, WorkItem, Event, DefaultAppModel]:
-    admin.site.register(m)
+admin.site.register(DefaultAppModel, DefaultAppModelAdmin)
 
